e2b57a148c1a66fa77cad35737549a9e4521bb79
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-24 23:47:50 +0530

    Update start.py

--- bot/plugins/commands/start.py
+++ bot/plugins/commands/start.py
@@ -8,6 +8,4 @@ async def start_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
         text=f"Hey {message.from_user.first_name},{START}{SOURCE}",
-        reply_to_message_id=message.id,
-        enums.ParseMode.HTML
-)
+        reply_to_message_id=message.id)
