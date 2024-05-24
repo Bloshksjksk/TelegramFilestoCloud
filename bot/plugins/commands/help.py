6391dbcd4cecd38f91afe5af691281336378d572
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-24 23:06:25 +0530

    Update help.py

--- bot/plugins/commands/help.py
+++ bot/plugins/commands/help.py
@@ -8,5 +8,4 @@ async def help_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
         text=f"Hey {message.from_user.first_name},{HELP}{SOURCE}",
-        reply_to_message_id=message.id,
-        enums.ParseMode.HTML)
+        reply_to_message_id=message.id)
